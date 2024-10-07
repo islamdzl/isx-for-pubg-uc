9d0c58abcc3afee1c2d7cc56a4c6032672561bcb
IslamDZl <157886980+islamdzl@users.noreply.github.com>
2024-10-07 21:08:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ var databisget =(async()=>{
 })
 // app.use(express.static('./UC'))
 app.get('/tiktok',(req,res)=>{
-    res.send('tiktok-developers-site-verification=0ueza5izPD7fV8jqWRDJEdQk5C1iSrZ0')
+    res.sendFile('tiktok-developers-site-verification=0ueza5izPD7fV8jqWRDJEdQk5C1iSrZ0')
 })
 app.post('/onuc',(req,res)=>{
     var ID = req.body.ID
